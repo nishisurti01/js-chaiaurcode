b79fa3212d5548bc349733945f706f7239cc9872
nishisurti01 <164928806+nishisurti01@users.noreply.github.com>
2024-07-11 22:06:20 +0000

    Data Types Conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = 33
+
+console.log(typeof score)
+
+scoreIntoStrings = "33A"
+
+let scoreIntonumber = Number(scoreIntoStrings)
+console.log(typeof scoreIntoStrings)
+console.log(scoreIntonumber)
+
+// number => Number 
+// string+number => NaN
+// true=>1
+
+let isLoggedIn = 1
+let booleanIsValue = Boolean(isLoggedIn)
+console.log(booleanIsValue)
+
+// 1=> true 0=> false
+// " " => false "1"=> true 
