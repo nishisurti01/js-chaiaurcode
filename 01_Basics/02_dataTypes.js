3366ddb782b9b043c44e3e71cabf6b5d98345313
nishisurti01 <164928806+nishisurti01@users.noreply.github.com>
2024-07-11 21:52:20 +0000

    dataTypes Done

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all js code as newer version 
+//code readability 
+
+//URL: https://tc39.es/ for excma Script which will declare standards for JS
+
+
+
+let name="nishi" // string
+let age = 18 // Number 
+let isloggedIn = false // Bollean 
+
+/* DataTypes : 
+
+Primitives 
+
+1. string 
+2. boolean 
+3. null => stand alone value 
+4. undefined // place holder JS
+5. symbols ==> Uniquenss 
+
+Non-Premetives 
+
+1.Object 
+2. Array */
+
+
+console.log(typeof(null)) //object 
+console.log(typeof(undefined)) //undefined
+
